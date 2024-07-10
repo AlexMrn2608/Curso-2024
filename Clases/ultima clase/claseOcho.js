7b26a6a4000f2efa9ab3b12c58339421b87bed6b
Alex Moreno <alexmrn2608@gmail.com>
2024-07-09 21:04:26 -0300

    first commit

---
+++ Clases/ultima clase/claseOcho.js
@@ -0,0 +1,34 @@
+// 1. Variables y Tipos de datos:
+
+// let numero = 10;
+// let texto = "Hola Mundo";
+
+// function promedio(a,b){
+//     return (a+b)/2;
+// }
+
+// let edad = 28;
+
+// if (edad >= 18){
+//     console.log("Eres mayor de edad");
+// }
+// else{
+//     console.log("Eres menor de edad");
+// }
+
+
+// for(let i = 0; i < 10; 1++){
+//     console.log("itteracion" + i);
+// }
+
+console,log("aaa")
+
+
+
+
+
+
+
+
+
+
